7346f800d98c4c325e98ad9fc5fe1a57802b5a3e
muhammadannabil21-cloud <muhammadannabil21@gmail.com>
2026-06-25 14:41:55 +0800

    Selesai Bagian 3: Multi-Class

---
+++ Buku.java
@@ -0,0 +1,41 @@
+package bagian3.perpustakaan;
+
+public class Buku {
+
+    private String judul;
+    private String penulis;
+    private boolean dipinjam;
+    private int tahunTerbit; // Latihan Mandiri No.2
+
+    public Buku(String judul, String penulis, int tahunTerbit) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.dipinjam = false;
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public boolean isDipinjam() {
+        return dipinjam;
+    }
+
+    public void setDipinjam(boolean dipinjam) {
+        this.dipinjam = dipinjam;
+    }
+
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public String info() {
+        String status = dipinjam ? "Dipinjam" : "Tersedia";
+        return judul + " oleh " + penulis + " (" + tahunTerbit + ") [" + status + "]";
+    }
+}
